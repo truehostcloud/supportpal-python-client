bfb65fb909299a9a98b6048dfef2b353e8f22174
willymwai <willymwaid92@gmail.com>
2026-08-04 08:02:00 +0300

    Fix installed client imports

--- supportpal_client/api/__init__.py
+++ supportpal_client/api/__init__.py
@@ -1,8 +1,7 @@
 # flake8: noqa
 
 # import apis into api package
-from api.feedback_api import FeedbackApi
-from api.message_api import MessageApi
-from api.operator_api import OperatorApi
-from api.ticket_api import TicketApi
-
+from supportpal_client.api.feedback_api import FeedbackApi
+from supportpal_client.api.message_api import MessageApi
+from supportpal_client.api.operator_api import OperatorApi
+from supportpal_client.api.ticket_api import TicketApi
